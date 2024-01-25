dcd3fde036c3d9d3c4ff45135618acbce563c462
hurtaditoo <al415507@uji.es>
2024-01-25 12:25:34 +0100

    sum - Version 1

---
+++ Sum/sum.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+  
+    int number;
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+
+    int sum = 0;
+    
+    std::cout << "The sum from 1 to " << number << " is: " << sum << std::endl;
+
+    return 0; 
+}
+
