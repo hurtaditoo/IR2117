7de9d720abd47340f81e6be5eb2f961d43b7fdd4
hurtaditoo <al415507@uji.es>
2024-01-25 12:07:49 +0100

    Ask username

--- HelloWorld/hello.cc
+++ HelloWorld/hello.cc
@@ -1,7 +1,9 @@
 #include <iostream>
 
-int main()
-{
-	std::cout << "Hello world!" << std::endl;
+int main() {
+	std::cout << "What is your name?" << std::endl;
+	std::string name;
+	std::cin >> name;
+	std::cout << "Hello " << name <<  "!" << std::endl;
 	return 0;
 }
