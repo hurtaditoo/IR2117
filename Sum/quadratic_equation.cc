ff31d3b9dc598fd44578c5d7dcae636852e143ab
hurtaditoo <al415507@uji.es>
2024-01-25 16:39:04 +0100

    quadratic_equation - Version 1

---
+++ Sum/quadratic_equation.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+
+    int a, b, c;
+
+    std::cout << "Solving a quadratic ecuation" << std::endl;
+    
+    std::cout << "Coefficient A: ";
+    std::cin >> a;
+    
+    std::cout << "Coefficient B: ";
+    std::cin >> b;
+     
+    std::cout << "Coefficient C: ";
+    std::cin >> c;
+
+    int solution = 0;
+
+    std::cout << "The solution is: " << solution << std::endl;
+
+    return 0;
+}
