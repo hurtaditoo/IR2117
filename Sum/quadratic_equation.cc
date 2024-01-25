ad1120b19520903ff6552c37c55b24c57bcb61f4
hurtaditoo <al415507@uji.es>
2024-01-25 17:08:28 +0100

    quadratic_equation - Version 4

--- Sum/quadratic_equation.cc
+++ Sum/quadratic_equation.cc
@@ -18,15 +18,19 @@ int main() {
 
     int discriminante = (b * b) - (4 * a * c);
 
-    if (discriminante>=0) {
+    if (discriminante > 0) {
         double sol1 = (-b + std::sqrt(discriminante)) / (2 * a);
         double sol2 = (-b - std::sqrt(discriminante)) / (2 * a);
 
         std::cout << "Two solutions: " << sol1 << " and " << sol2 << std::endl;
-    }    
-    else {
-        std::cout << "There is no solution" << std::endl;
+    } 
+    else if (discriminante == 0) {
+        double sol = -b / (2 * a);
+        std::cout << "One real solution: " << sol << std::endl;
     }
+    else {
+        std::cout << "No real solutions." << std::endl;
+    } 
 
     return 0;
 }
