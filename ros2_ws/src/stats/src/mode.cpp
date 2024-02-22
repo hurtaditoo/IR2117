5ffad292c65460efc5fed754501949fef6e9aef7
hurtaditoo <al415507@uji.es>
2024-02-22 13:29:30 +0100

    Version - 10 a medias

--- ros2_ws/src/stats/src/mode.cpp
+++ ros2_ws/src/stats/src/mode.cpp
@@ -1,40 +1,34 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/int32.hpp"
 #include "std_msgs/msg/float32.hpp"
+
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <map>
 
-double median;
+std::vector<float> numbers;
+std::map<int, int> dic;
 
-std::vector<int> numbers;
 rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr publisher;
 
 void topic_callback(const std_msgs::msg::Int32::SharedPtr msg)
 {
-	numbers.push_back(msg->data);
-  std::sort(numbers.begin(), numbers.end());
-  for (std::size_t i=0; i < numbers.size(); i++) {
-  	std::cout << numbers[i];
-  }
+	int number = msg->data;
+		numbers[number]++;
 
-  if (numbers.size() % 2 == 0) {
-      median = (numbers[numbers.size() / 2 - 1] + numbers[numbers.size() / 2]) / 2.0;
-  } else {
-      median = numbers[numbers.size() / 2];
-  }
-
-	std_msgs::msg::Float32 median_msg;
-  median_msg.data = median;
-  publisher->publish(median_msg);
+	std_msgs::msg::Float32 mode_msg;
+  mode_msg.data = numbers[number];
+  publisher->publish(mode_msg);
 }
 
 int main(int argc, char * argv[])
 {
 	rclcpp::init(argc, argv);
-	auto node = rclcpp::Node::make_shared("median");
+	auto node = rclcpp::Node::make_shared("mode");
 	auto subscription = 
 		node->create_subscription<std_msgs::msg::Int32>("number", 10, topic_callback);
-  publisher = node->create_publisher<std_msgs::msg::Float32>("median", 10);
+  publisher = node->create_publisher<std_msgs::msg::Float32>("mode", 10);
 	rclcpp::spin(node);
  	rclcpp::shutdown();
   return 0;
