fc7aa118b89ba9497500fe34a26984d6c15cce51
hurtaditoo <al415507@uji.es>
2024-02-25 23:30:29 +0100

    Version - 12 final

--- ros2_ws/src/stats/src/mode.cpp
+++ ros2_ws/src/stats/src/mode.cpp
@@ -1,6 +1,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/int32.hpp"
 #include "std_msgs/msg/float32.hpp"
+#include "std_msgs/msg/float32_multi_array.hpp"
 
 #include <iostream>
 #include <vector>
@@ -10,21 +11,28 @@
 std::vector<float> numbers;
 std::map<int, int> dic;
 int max = 0;
+std::vector<int> mode;
 
-rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr publisher;
+rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr publisher;
 
 void topic_callback(const std_msgs::msg::Int32::SharedPtr msg)
 {
 	numbers.push_back(msg->data);
 	int number = msg->data;
-	for (std::size_t i=0; i < numbers.size(); i++) {
-		if (dic[numbers[i] > max) {
-			max = numbers[number]++;
+	numbers[number]++;
+	for (auto i : dic) {
+		if (i.second > max) {
+			max = i.second;
 		}
 	}
 
-	std_msgs::msg::Float32 mode_msg;
-  mode_msg.data = numbers[number];
+	for (auto i : dic) {
+		if (i.second == max) {
+			mode.push_back(i.first);
+		}
+	}
+	std_msgs::msg::Float32MultiArray mode_msg;
+  mode_msg.data = mode;
   publisher->publish(mode_msg);
 }
 
@@ -34,7 +42,7 @@ int main(int argc, char * argv[])
 	auto node = rclcpp::Node::make_shared("mode");
 	auto subscription =
 		node->create_subscription<std_msgs::msg::Int32>("number", 10, topic_callback);
-  publisher = node->create_publisher<std_msgs::msg::Float32>("mode", 10);
+  publisher = node->create_publisher<std_msgs::msg::Float32MultiArray>("mode", 10);
 	rclcpp::spin(node);
  	rclcpp::shutdown();
   return 0;
