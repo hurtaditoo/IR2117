f5ddb012033aa7e656abc840198a28c63d0730fe
hurtaditoo <al415507@uji.es>
2024-02-26 11:50:14 +0100

    Version -final

--- minmax/src/minmax.cpp
+++ minmax/src/minmax.cpp
@@ -9,31 +9,40 @@
 std::vector<int> numbers;
 std::map<int, int> dic;
 int max = 0;
+std::vector<int> minmax;
 
-rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr publisher;
+rclcpp::Publisher<std_msgs::msg::Int32MultiArray>::SharedPtr publisher;
 
 void topic_callback(const std_msgs::msg::Int32::SharedPtr msg)
 {	
 	numbers.push_back(msg->data);
 	int number = msg->data;
-	for (std::size_t i=0; i < numbers.size(); i++) {
-  	if (dic[numbers[i]] > max) {
-			max = numbers[number]++;
+	int min = number;
+	numbers[number]++;
+	for (auto i : dic) {
+		if (i.second > max) {
+			max = i.second;
 		}
 	}
 
-	std_msgs::msg::Int32 max_msg;
-  max_msg.data = numbers[number];
-	publisher->publish(max_msg);
+	for (auto i : dic) {
+		if (i.second < min) {
+			min = i.first;
+		}
+	}
+
+	std_msgs::msg::Int32MultiArray minmax_msg;
+  minmax_msg.data = numbers[number];
+	publisher->publish(minmax_msg);
 }
 
 int main(int argc, char * argv[])
 {
 	rclcpp::init(argc, argv);
-	auto node = rclcpp::Node::make_shared("max");
+	auto node = rclcpp::Node::make_shared("minmax");
 	auto subscription = 
 		node->create_subscription<std_msgs::msg::Int32>("number", 10, topic_callback);
-	publisher = node->create_publisher<std_msgs::msg::Int32>("max", 10);
+	publisher = node->create_publisher<std_msgs::msg::Int32MultiArray>("minmax", 10);
 	rclcpp::spin(node);
  	rclcpp::shutdown();
   return 0;
