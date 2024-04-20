4eeb7739382f23ff07d0c32e07bb943db83314ee
hurtaditoo <al415507@uji.es>
2024-04-20 16:34:37 +0200

    Optimizations detector.cpp

--- tb3_ws/src/obstacles/src/detector.cpp
+++ tb3_ws/src/obstacles/src/detector.cpp
@@ -16,11 +16,15 @@ void callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
 	float angle = msg->angle_min;
   for (float range: msg->ranges) 
   {
-    if (angle > M_PI ) angle -= 2*M_PI;
+    if (angle > M_PI ) 
+    	angle -= 2*M_PI;
 		if((angle >= obs_angle_min) and (angle <= obs_angle_max))
 		{
 			if (range <= obs_threshold)
+			{
 				out_msg.data = true;
+				break;
+			}
 		}
     angle += msg->angle_increment;
   }
