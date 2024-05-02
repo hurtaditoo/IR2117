b6cbf0e9cd2893865064114ed940f89ded320baf
hurtaditoo <al415507@uji.es>
2024-05-02 22:07:11 +0200

    Add the “radius” parameter
    Incompleta

--- tb3_ws/src/olympic/src/rings.cpp
+++ tb3_ws/src/olympic/src/rings.cpp
@@ -11,11 +11,14 @@ int main(int argc, char * argv[])
  rclcpp::init(argc, argv);
  auto node = rclcpp::Node::make_shared("rings");
  auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel", 10);
+ node->declare_parameter("radius", 1.0);
+ node->declare_parameter("angular_speed", 1.0);
  geometry_msgs::msg::Twist message;
  rclcpp::WallRate loop_rate(500ms);
 
- double radius = 1.0;
- double angular_speed = 1.0;
+ double radius = node->get_parameter("radius").get_parameter_value().get<double>();
+ double angular_speed = node->get_parameter("angular_speed").get_parameter_value().get<double>();
+
  int k=0;
  double m = (2 * M_PI * radius) / (angular_speed * 0.01);
  while (rclcpp::ok() && k<m) {
